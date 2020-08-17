92f9e4e67cb4e0d173c42219ce9bcbc7b1503145
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-17 22:45:10 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -12,7 +12,7 @@ class Pet extends React.Component {
             PET NAME
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
             <p>Age: {this.props.pet.age}</p>
